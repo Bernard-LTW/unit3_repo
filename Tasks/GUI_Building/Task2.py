6b154cf0e944180881220873b8f685111d7639da
Bernard-LTW <34589202+Bernard-LTW@users.noreply.github.com>
2023-01-29 15:35:18 +0900

    2023-01-29 guitask update1

--- Tasks/GUI_Building/Task2.py
+++ Tasks/GUI_Building/Task2.py
@@ -6,11 +6,21 @@ class task2(MDApp):
     def build(self):
         return
     def byte2bit(self):
-        self.output = float("{:.2f}".format(int(self.root.ids.byte_in.text)*8))
-        self.root.ids.output_label.text = f"{self.output} bits"
+        if self.root.ids.byte_in.text.isdigit():
+            self.output = float("{:.2f}".format(int(self.root.ids.byte_in.text)*8))
+            self.root.ids.output_label.text = f"{self.output} bits"
+        else:
+            self.root.ids.output_label.text = "Invalid Input"
+            self.root.ids.byte_in.text = ""
+            self.root.ids.output_label.text_color = (1,0,0,1)
     def bit2byte(self):
-        self.output = float("{:.2f}".format(int(self.root.ids.bit_in.text)/8))
-        self.root.ids.output_label.text = f"{self.output} bytes"
+        if self.root.ids.bit_in.text.isdigit():
+            self.output = float("{:.2f}".format(int(self.root.ids.bit_in.text)/8))
+            self.root.ids.output_label.text = f"{self.output} bytes"
+        else:
+            self.root.ids.output_label.text = "Invalid Input"
+            self.root.ids.bit_in.text = ""
+            self.root.ids.output_label.text_color = (1,0,0,1)
 
 hey = task2()
 hey.run()
